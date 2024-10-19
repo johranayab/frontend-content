3b0c75b01befff8470e9f0a264647682600cedc3
johra_01 <148644216+johranayab@users.noreply.github.com>
2024-10-19 11:57:50 +0530

    Add files via upload

---
+++ thiskeyword/this.js
@@ -0,0 +1,41 @@
+// "use strict";
+
+// function johra(){
+//     console.log(this);
+    
+// }
+// johra();
+
+// " strict mood"
+// function johra(){
+//     console.log(this);
+    
+// }
+// johra();
+
+
+// const person={
+//     name:"johra",
+//     age:"21",
+//     greet:function(){
+//         console.log(`hwllo i am ${this.name}  andmy age is ${this.age}`);
+        
+//     }
+// };
+// console.log(person);
+
+// person.greet();
+// console.log(person);
+
+
+
+
+
+//new
+"use strict";
+function johra(){
+    console.log(this);
+    
+}
+johra();
+
